3ca31e045246cd0e25080a65ee7ad7df536e65b7
faust174 <m.soloninko@dev-branch.com>
2024-04-12 11:43:44 +0300

    7800.main: Created migration file to change files into media. Created custom plugin that checks if link contains unnecessary part and to removes it

---
+++ web/modules/custom/migration/src/Plugin/migrate/process/CustomLinkPlugin.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Drupal\migration\Plugin\migrate\process;
+
+use Drupal\migrate\MigrateExecutableInterface;
+use Drupal\migrate\ProcessPluginBase;
+use Drupal\migrate\Row;
+
+/**
+ * Removes links from body if they match a specific URL.
+ *
+ * @MigrateProcessPlugin(
+ *   id = "custom_link_plugin"
+ * )
+ */
+class CustomLinkPlugin extends ProcessPluginBase {
+  public function transform($value, MigrateExecutableInterface $migrate_executable, Row $row, $destination_property) {
+    $callback = function ($matches) {
+      $url = $matches[2];
+      $modified_url = str_replace('ehistory.osu.edu', '', $url);
+      return $matches[1] . $modified_url . $matches[3];
+    };
+
+    $pattern = '/(<a[^>]+href=["\'])([^"\']+ehistory\.osu\.edu\/[^"\']*)(["\'][^>]*>)/i';
+
+    return preg_replace_callback($pattern, $callback, $value);
+  }
+
+}
